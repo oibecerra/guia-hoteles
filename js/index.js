85a767d4aa406552de1589d059ec564418ef7530
Oliver Becerra A <obecerra@conservador.cl>
2024-03-04 22:36:22 -0300

    Módulo 4. final.

--- js/index.js
+++ js/index.js
@@ -6,12 +6,21 @@ $(function(){
     });
 
     $('#contacto').on('show.bs.modal', function (e){
+    console.log('show');
         $('#contactoBtn').removeClass('btn-outline-success');
         $('#contactoBtn').addClass('btn-primary');
         $('#contactoBtn').prop('disabled', true);
     });
+     $('#contacto').on('shown.bs.modal', function (e){
+        console.log('shown');
+        });
+
+    $('#contacto').on('hide.bs.modal', function (e){
+        console.log('hide');
+    });
 
     $('#contacto').on('hidden.bs.modal', function (e){
+    console.log('hidden');
         $('#contactoBtn').removeClass('btn-primary');
         $('#contactoBtn').addClass('btn-outline-success');
         $('#contactoBtn').prop('disabled', false);
